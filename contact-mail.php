6bbedc03b64b51f6633d51a35c106cde264e5319
Dioulette <aude.arnaudet@gmail.com>
2021-10-26 14:34:04 +0200

    test formulaire

--- contact-mail.php
+++ contact-mail.php
@@ -1,22 +1,42 @@
 <?php
+$nom = $_POST["nom"] ;
+$prenom = $_POST["prenom"] ;
+$email = $_POST["email"] ;
+$telephone = $_POST["telephone"] ;
+$message = $_POST["message"];
 
-	$prenom = $_POST['prenom'];
-	$nom = $_POST['nom'];
-	$email = $_POST['email'];
-	$telephone = $_POST['telephone'];
-	$message = $_POST['message'];
-	
-	$email_from = 'contact@xxx';
-	$email_body = "Nouveau message de $prenom $nom\n\n".
-		"Son adresse email: $email\n\n".
-        "Son téléphone: $telephone\n\n"
-		"Son message:\n $message\n\n\n".
-		
-	$to = 'contact@xxx';
-	$headers = "De $email_from \r\n";
-		
-	
-	mail($to,$email_subject,$email_body,$headers);
-	
-	header("refresh:5;url=index.html");
+$EmailTo = "aude.arnaudet@gmail.com" ;
+$Subject = "Nouveau message reçu" ;
+
+// préparer le corps du texte de l'e-mail
+$Corps .= "Nom : " ;
+$Corps .= $nom;
+$Corps .= "\n";
+
+$Corps .= "Prénom : " ;
+$Corps .= $prenom;
+$Corps .= "\n";
+
+$Corps .= "Télephone : " ;
+$Corps .= $telephone;
+$Corps .= "\n";
+
+$Body .= "E-mail : " ;
+$Corps .= $email;
+$Corps .= "\n";
+
+$Corps .= "Message : " ;
+$Corps .= $message;
+$Corps .= "\n";
+
+// envoyer un e-mail
+$success = mail($EmailTo, $Subject, $Body, "From :".$email);
+
+// redirige vers la page de réussite
+si ($succès){
+   echo "succès" ;
+}autre{
+    echo "invalide" ;
+}
+
 ?>
